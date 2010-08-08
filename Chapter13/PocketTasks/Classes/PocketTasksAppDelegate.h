a142097604078cd826bf1f875f9e4f7187b26aa0
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2010-08-08 18:09:28 +0200

    chapter 13 skeleton

---
+++ Chapter13/PocketTasks/Classes/PocketTasksAppDelegate.h
@@ -0,0 +1,31 @@
+//
+//  PocketTasksAppDelegate.h
+//  PocketTasks
+//
+//  Created by Johannes Fahrenkrug on 05.08.10.
+//  Copyright Springenwerk 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface PocketTasksAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
---
+++ Chapter13/PocketTasks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PocketTasks
+//
+//  Created by Johannes Fahrenkrug on 05.08.10.
+//  Copyright Springenwerk 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
