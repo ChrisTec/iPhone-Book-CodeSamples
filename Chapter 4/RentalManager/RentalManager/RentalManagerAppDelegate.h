d4c2b9ccba2b700c4ce1a263f9267801e04d16a2
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2011-09-02 11:36:55 +0200

    checked chapter 4

--- Chapter 4/RentalManager/RentalManager/RentalManagerAppDelegate.h
+++ Chapter 4/RentalManager/RentalManager/RentalManagerAppDelegate.h
@@ -2,18 +2,18 @@
 //  RentalManagerAppDelegate.h
 //  RentalManager
 //
-//  Created by Christopher Fairbairn on 19/03/10.
-//  Copyright ChrisTec Limited 2010. All rights reserved.
+//  Created by Amos Bannister on 10/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
 @interface RentalManagerAppDelegate : NSObject <UIApplicationDelegate> {
-    
-    UIWindow *window;
-    UINavigationController *navigationController;
+
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
+
 @property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
 
 @end
-
---
+++ Chapter 4/RentalManager/RentalManager/RootViewController.m
@@ -0,0 +1,171 @@
+//
+//  RootViewController.m
+//  RentalManager
+//
+//  Created by Amos Bannister on 10/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+
+RentalProperty properties[] = {
+    { @"13 Waverly Crescent, Sumner", TownHouse, 420.0f },
+    { @"74 Roberson Lane, Christchurch", Unit, 365.0f },
+    { @"17 Kipling Street, Riccarton", Unit, 275.9f },
+    { @"4 Everglade Ridge, Sumner", Mansion, 1500.0f },
+    { @"19 Islington Road, Clifton", Mansion, 2000.0f }
+};
+
+@implementation RootViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"CityMappings" ofType:@"plist"];
+    cityMappings = [[NSDictionary alloc] initWithContentsOfFile:path];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+/*
+ // Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	// Return YES for supported orientations.
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+ */
+
+// Customize the number of sections in the table view.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return ARRAY_SIZE(properties);
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+    RentalProperty *details = &properties[indexPath.row];
+    
+    int indexOfComma = [details->address rangeOfString:@","].location;
+    NSString *address = [details->address substringToIndex:indexOfComma];
+    NSString *city = [details->address substringFromIndex:indexOfComma + 2];
+
+    cell.textLabel.text = address;
+    
+    NSString *imageName = [cityMappings objectForKey:city];
+    cell.imageView.image = [UIImage imageNamed:imageName];
+    
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Rents for $%0.2f per week", properties[indexPath.row].weeklyRentalPrice];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        // Delete the row from the data source.
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+    else if (editingStyle == UITableViewCellEditingStyleInsert)
+    {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    /*
+    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+    // ...
+    // Pass the selected object to the new view controller.
+    [self.navigationController pushViewController:detailViewController animated:YES];
+    [detailViewController release];
+	*/
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Relinquish ownership any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+
+    // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
+    // For example: self.myOutlet = nil;
+}
+
+- (void)dealloc
+{
+    [cityMappings release];
+    
+    [super dealloc];
+}
+
+@end
